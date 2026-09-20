2038d02f1066b4cafd08dc8d52b7025261f6b58d
ghstayagstudent-droid <ghstayag.student@ua.edu.ph>
2026-09-20 20:50:36 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! I am Gin Hanner Tayag, 20 years old. I love driving and playing online games. My favorite song is Suffering by Jorge Rivera-Herrans, Anna Lea, and my favorite food is Pork Sinigang. In 5 years, I see myself as a successful man living a happy life, working as a Junior Data Analyst in a great company.</p>
-        <p>
+        <p>Hello! I am Gin Hanner Tayag, 20 years old. I love driving and playing online games. My favorite song is Suffering by Jorge Rivera-Herrans, Anna Lea, and my favorite food is Pork Sinigang.In 5 years, I see myself living a peaceful and happy life working as a valued Data Analyst in a great company culture while having the time and freedom to pursue my personal passions
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
