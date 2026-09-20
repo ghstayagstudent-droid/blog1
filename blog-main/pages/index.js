827fe36e5694babdb6a5d01ae72ad69e606cfda4
unknown <ghstayag.student@ua.edu.ph>
2026-09-20 20:33:23 +0800

    Publish personal profile website

---
+++ blog-main/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+ 
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello! I am Gin Hanner Tayag, 20 years old. I love driving and playing online games. My favorite song is Suffering by Jorge Rivera-Herrans, Anna Lea, and my favorite food is Pork Sinigang. In 5 years, I see myself as a successful man living a happy life, working as a Junior Data Analyst in a great company.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
